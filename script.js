b656000ceafffe9c5e6b0f5a62cd5c6082a65d37
ReneSzz <renesuar99@gmail.com>
2023-01-13 21:36:20 -0500

    put code back into method

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ constructor(name)
 fillTasks(){
     this.tasks.forEach(element => {
       
-       
+        let taskContainer = document.createElement('div');
+        taskContainer.classList.add("taskBar")
+        let taskName = document.createElement('h3');
+        let deleteTask = document.createElement('h4');
+        
+        deleteTask.innerText = "x";
+        taskName.innerText = this.name;
+        
+        container.appendChild(taskContainer);
+        taskContainer.appendChild(taskName);
+        taskContainer.appendChild(deleteTask);
+        
 
 
         
@@ -21,17 +32,6 @@ fillTasks(){
 }
 }
 
-let taskContainer = document.createElement('div');
-taskContainer.classList.add("taskBar")
-let taskName = document.createElement('h3');
-let deleteTask = document.createElement('h4');
-
-deleteTask.innerText = "x";
-taskName.innerText = "swag";
-
-container.appendChild(taskContainer);
-taskContainer.appendChild(taskName);
-taskContainer.appendChild(deleteTask);
 
 
 
