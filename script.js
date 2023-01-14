4ed41e7b832f57e44af38e94a8e8b9bf1518cc7b
ReneSzz <renesuar99@gmail.com>
2023-01-13 20:37:28 -0500

    added new style class

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let container = document.getElementById('container')
+
+
 
 class todoList {
     name = "";
@@ -10,16 +13,26 @@ constructor(name)
 fillTasks(){
     this.tasks.forEach(element => {
       
-        let taskContainer = document.createElement('div');
-        let taskName = document.createElement('h3');
-        let deleteTask = document.createElement('h4');
-
-        taskName.innerText = element;
-
-
+       
 
 
         
     });
 }
 }
+
+let taskContainer = document.createElement('div');
+taskContainer.classList.add("taskBar")
+let taskName = document.createElement('h3');
+let deleteTask = document.createElement('h4');
+
+deleteTask.innerText = "x";
+taskName.innerText = "swag";
+
+container.appendChild(taskContainer);
+taskContainer.appendChild(taskName);
+taskContainer.appendChild(deleteTask);
+
+
+
+
