5410eae7b2ca55ce070e9cd38476e7d20dc00a3a
ReneSzz <renesuar99@gmail.com>
2023-02-13 16:33:44 -0500

    added basic idea to create new lists

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
 let container = document.getElementById('container');
-let newList = document.getElementById('newList');
+let createNewList = document.getElementById('newList');
 let subHeading2 = document.getElementById('subHeading2');
-newList.style.color='red';
+createNewList.style.color='red';
 
 let todoLists = []
 
@@ -17,49 +17,79 @@ constructor(name)
     this.name = name;
 }
 
+
+newListCreation(){
+// Create Template
+
+}
+
+
+
+
 fillTasks(){
-    this.tasks.forEach(element => {
+    
       
-        let taskContainer = document.createElement('div');
-        taskContainer.classList.add("taskBar")
-        let taskName = document.createElement('h3');
-        let deleteTask = document.createElement('h4');
+        // let taskContainer = document.createElement('div');
+        // taskContainer.classList.add("taskBar")
+        // let taskName = document.createElement('h3');
+        // let deleteTask = document.createElement('h4');
+        let test = document.createElement('h1');
+        test.innerText =" Test header";
+        // deleteTask.innerText = "x";
+        // taskName.innerText = this.name;
         
-        deleteTask.innerText = "x";
-        taskName.innerText = this.name;
-        
-        container.appendChild(taskContainer);
-        taskContainer.appendChild(taskName);
-        taskContainer.appendChild(deleteTask);
+        container.appendChild(test);
+        // taskContainer.appendChild(taskName);
+        // taskContainer.appendChild(deleteTask);
+        console.log(this.todoList);
         
 
 
         
-    });
-}
-}
-
-newList.addEventListener('click', function (e)
-{   
-    // Create Template
-    let newListItem = document.createElement('div');
-    newListItem.classList.add('listSideBar')
-    let listNameLabel = document.createElement('input');
-    let acceptButton = document.createElement('button');
    
-    // ACCEPT Button
-    acceptButton.classList.add('acceptButton')
-    acceptButton.innerText = "Accept"
-    
-    
-    // adding items 
-    subHeading2.appendChild(newListItem)
-     newListItem.appendChild(listNameLabel)
-     newListItem.appendChild(acceptButton);
-     listNameLabel.style.color='black';
-    newList = new todoList(newListName.innerText);
-    todoLists[todoLists.length]=newList;
-    console.log(todoLists);
+}
+}
+
+createNewList.addEventListener('click', function (e)
+{   
+
+
+let newListItem = document.createElement('div');
+newListItem.classList.add('listSideBar')
+let listNameLabel = document.createElement('input');
+let listName = document.createElement('p');
+let acceptButton = document.createElement('button');
+listName.innerText = "Enter New List Title";
+
+// ACCEPT Button
+acceptButton.classList.add('acceptButton')
+acceptButton.innerText = "Accept"
+
+
+// adding items 
+subHeading2.appendChild(newListItem)
+ newListItem.appendChild(listName);
+ newListItem.appendChild(listNameLabel)
+ newListItem.appendChild(acceptButton);
+ listNameLabel.style.color='black';
+//  newList = new todoList(listName.innerText);
+// todoLists[todoLists.length]=newList;
+// console.log(todoLists);2
+
+acceptButton.addEventListener('click', function (e)
+{
+    listName.innerText = listNameLabel.value;
+    newListItem.removeChild(acceptButton);
+    newListItem.removeChild(listNameLabel);
+    newListItem.classList.remove("listSideBar");
+    newList = new todoList (listName.innerText);
+    console.log(newList);
+    newList.fillTasks();
+   
+}) 
+
+
+  
 
 });
 
